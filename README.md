55c9b756eac0709bf73b03d88f94817c8b2c8d87
Alexandre ABRIOUX <alexandre-abrioux@users.noreply.github.com>
2024-05-22 12:09:34 +0200

    chore: migrate to TheGraph Studio

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # Price Aggregators
 
-This subgraph indexes the price aggregators updates for easy query
+This subgraph indexes the price aggregators updates for easy query.
 
 ## Deploy a subgraph
 
-1. Create the subgraph on TheGraph hosted service
-2. clone this repo
-3. `yarn`
-4. `yarn codegen subgraph.fantom.yaml`
-5. `TOKEN=YOUR_TOKEN ./scripts/deploy.sh fantom`
+1. Create the subgraph on TheGraph Studio. Follow the naming `price-aggregators-[chain]`
+2. Clone this repo
+3. Run `yarn`
+4. Run `DEPLOY_KEY=[subgraph_studio_deploy_key] ./scripts/deploy.sh [chain]`
--- scripts/prepare.ts
+++ scripts/prepare.ts
@@ -1,10 +1,7 @@
 import mustache from "mustache";
 import fs from "fs";
 import path from "path";
-import {
-  chainlinkConversionPath,
-  DeploymentInformation
-} from "@requestnetwork/smart-contracts";
+import {chainlinkConversionPath, DeploymentInformation} from "@requestnetwork/smart-contracts";
 
 type DataSource = {
   version: string;
@@ -18,36 +15,48 @@ const deployments: Record<
   { deployment: Record<string, DeploymentInformation>; abi: any[] }
 > = (chainlinkConversionPath as any).info;
 
-const templates: Record<string, DataSource[]> = {};
+const getTheGraphChainName = (chainName: string) => {
+  switch (chainName) {
+    case "xdai":
+      return "gnosis";
+    case "zksyncera":
+      return "zksync-era";
+    default:
+      return chainName;
+  }
+};
 
-for (const [v, { deployment, abi }] of Object.entries(deployments)) {
-  const version = v.split(".").join("_");
-  fs.writeFileSync(
-    path.join(__dirname, `../abis/AggregatorContract-${version}.json`),
-    JSON.stringify(abi, null, 2)
-  );
-  for (const [network, { address, creationBlockNumber }] of Object.entries(
-    deployment
-  )) {
-    if (!templates[network]) templates[network] = [];
-    templates[network].push({
-      address,
-      creationBlockNumber,
-      network,
-      version
-    });
+const prepare = () => {
+  const templates: Record<string, DataSource[]> = {};
+
+  for (const [v, { deployment, abi }] of Object.entries(deployments)) {
+    const version = v.split(".").join("_");
+    fs.writeFileSync(
+        path.join(__dirname, `../abis/AggregatorContract-${version}.json`),
+        JSON.stringify(abi, null, 2)
+    );
+    for (const [network, { address, creationBlockNumber }] of Object.entries(
+        deployment
+    )) {
+      if(['rinkeby', 'arbitrum-rinkeby', 'goerli'].includes(network)) continue;
+      if (!templates[network]) templates[network] = [];
+      templates[network].push({
+        address,
+        creationBlockNumber,
+        network: getTheGraphChainName(network),
+        version
+      });
+    }
   }
 
   const template = fs
-    .readFileSync(path.join(__dirname, "../subgraph.template.yaml"))
-    .toString();
+      .readFileSync(path.join(__dirname, "../subgraph.template.yaml"))
+      .toString();
+
+  for (const [network, dataSources] of Object.entries(templates)) {
+    const result = mustache.render(template, { dataSources });
+    fs.writeFileSync(`subgraph.${network}.yaml`, result);
+  }
 }
 
-const template = fs
-  .readFileSync(path.join(__dirname, "../subgraph.template.yaml"))
-  .toString();
-
-for (const [network, dataSources] of Object.entries(templates)) {
-  const result = mustache.render(template, { dataSources });
-  fs.writeFileSync(`subgraph.${network}.yaml`, result);
-}
+prepare();
